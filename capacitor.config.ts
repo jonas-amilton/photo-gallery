7ace3d88f56fb79517299bb8e405257b24f3ff30
jonas-amilton <jonas96@edu.unisinos.br>
2024-11-08 23:57:03 -0300

    chore: configurando o capacitor para deploy no appflow

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,15 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.jonas.photogallery',
   appName: 'photo-gallery',
   webDir: 'www',
+  server: {
+    androidScheme: 'https',
+  },
   plugins: {
     LiveUpdates: {
-      appId: '042a1261',
+      appId: '851b7351',
       channel: 'Production',
       autoUpdateMethod: 'background',
       maxVersions: 2,
